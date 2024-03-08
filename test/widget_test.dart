c4a7790ba026999afd0b6776bb61706574442a76
UT404 <ukeshthapaliya10@gmail.com>
2024-03-08 23:26:25 +0545

    Added some splash screen package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:rent_onway/main.dart';
+import 'package:rent_onway/rent_app.dart';
 
 
 void main() {
